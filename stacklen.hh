4d16c184fe639c35187a39925f47ca0f0459b214
Georg Sauthoff <>
2010-06-22 15:05:33 +0200

    stacklen energy

--- stacklen.hh
+++ stacklen.hh
@@ -7,40 +7,70 @@
 #include <rtlib/table.hh>
 #include <rtlib/rna.hh>
 
+template <typename T>
+struct TA {
+  typedef Table::Quadratic<T, Table::CYK> array_t;
+  array_t &t;
+  TA(array_t &a) : t(a) {}
+
+  T &operator()(unsigned i, unsigned j)
+  { return t.get_tabulated(i, j); }
+  const T &operator()(unsigned i, unsigned j) const
+  { return t.get_tabulated(i, j); }
+  void operator()(unsigned i, unsigned j, const T &x)
+  { t.get_tabulated(i, j) = x; }
+};
+
 template <typename C, typename U>
 unsigned stacklen(const Basic_Sequence<C> &seq, U a, U b)
 {
-  static Table::Quadratic<unsigned, Table::CYK> array;
+  typedef Table::Quadratic<std::pair<int, unsigned>, Table::CYK> table_t;
+  static table_t table;
   static bool compute = true;
+  TA<std::pair<int, unsigned> > array(table);
   if (compute) {
-    array.init(seq, "stacklen");
+    table.init(seq, "stacklen");
     unsigned n = seq.size();
     for (unsigned j = 2; j<=n; ++j) {
       for (unsigned is = j-2+1; is > 0; --is) {
         unsigned i = is-1;
-        unsigned r = 0;
         if (j-i < 5) {
-          array.tabulate(i, j, r);
+          array(i, j).first = 0;
+          array(i, j).second = 0;
           continue;
         }
         if (basepairing(seq, i, j)) {
-          r = 1;
+          Subsequence s;
+          s.i = i;
+          s.j = j;
+          s.seq = &seq;
+          int nrg = sr_energy(s, s);
           if (j-i>3) {
-            unsigned t = array.get_tabulated(i+1, j-1) + r;
-            array.tabulate(i, j, t);
-          } else
-            array.tabulate(i, j, r);
-        } else
-          array.tabulate(i, j, r);
+            if (array(i+1, j-1).first + nrg > 0) { //array(i+1, j-1).first) {
+              array(i, j).first = 0;
+              array(i, j).second = 1;
+            } else {
+              array(i, j).first = array(i+1, j-1).first + nrg;
+              array(i, j).second = array(i+1, j-1).second + 1;
+            }
+          } else {
+            array(i, j).first = nrg;
+            array(i, j).second = 1;
+          }
+        } else {
+          array(i, j).first = 0;
+          array(i, j).second = 0;
+        }
       }
-      unsigned r = 0;
-      array.tabulate(j, j, r);
-      array.tabulate(j-1, j, r);
+      array(j, j).first = 0;
+      array(j, j).second = 0;
+      array(j-1, j).first = 0;
+      array(j-1, j).second = 0;
     }
-    unsigned r = 0;
-    array.tabulate(0, 0, r);
-    array.tabulate(0, 1, r);
-    compute = false;
+    array(0, 0).first = 0;
+    array(0, 0).second = 0;
+    array(0, 1).first = 0;
+    array(0, 1).second = 0;
 
 /*
     for (unsigned i = 0; i <= seq.size(); ++i) {
@@ -51,7 +81,7 @@ unsigned stacklen(const Basic_Sequence<C> &seq, U a, U b)
     }
     */
   }
-  return array.get_tabulated(a, b);
+  return array(a, b).second;
 }
 
 
