48a422681fd14cc6811433cb14bb9964105f742d
Stefan Janssen <stefan.m.janssen@gmail.com>
2010-12-09 17:11:51 +0100

    Alles fuer Pseudoknoten entfernt, so dass dieses Repository nurnoch fremde Grammatiken beinhaltet.

--- stacklenmain.cc
+++
@@ -1,9 +0,0 @@
-#include "stacklen.hh"
-
-int main(int argc, char **argv)
-{
-  Sequence seq(argv[1]);
-  char_to_rna(seq);
-  unsigned r = stacklen(seq, 0u, (unsigned)strlen(argv[1]));
-  std::cout << "Stacklen: " << r << '\n';
-}
