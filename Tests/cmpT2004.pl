827bd7b1f0f407ad92f211ca99d67fc45952e892
Stefan Janssen <stefan.m.janssen@gmail.com>
2012-04-29 18:14:57 +0200

    Testskript etwas erweitert.

--- Tests/cmpT2004.pl
+++ Tests/cmpT2004.pl
@@ -5,20 +5,22 @@ use warnings;
 use Data::Dumper;
 
 my $binVienna185 = '/home/sjanssen/Desktop/Turner2004/ViennaRNA-1.8.5/Progs/RNAsubopt';
-my $binVienna200 = '/home/sjanssen/Desktop/Turner2004/ViennaRNA-2.0.0/Progs/RNAsubopt';
-my $binGAPC = '/home/sjanssen/Desktop/LP/out';
-                
+my $binVienna200 = '/home/sjanssen/Desktop/Turner2004/ViennaRNA-2.0.5/Progs/RNAsubopt';
+my $binGAPC = '/home/sjanssen/Desktop/fold-grammars/out';
+my $ParamFile = '/home/sjanssen/Desktop/Turner2004/ViennaRNA-2.0.5/rna_turner2004.par';
+
 #~ my $sequence = "GCCUGCCACAGCCUAGCCUUAGCCUGGC";
 #~ my $sequence = "CUAGCCUUAGCCU";
-my ($version, $sequence) = @ARGV;
-die "usage: perl $0 <version = _1.8.5_ | 2.0.0> <sequence>\n" if (@ARGV != 2);
-$version = '1.8.5' if ($version ne '2.0.0');
+my ($version, $sequence, $dangle, $noLP, $grammar) = @ARGV;
+die "usage: perl $0 <version = _1.8.5_ | 2.0.5> <sequence>\n" if (@ARGV != 5);
+$version = '1.8.5' if ($version ne '2.0.5');
 
 my $call = "";
-$call = 'echo "'.$sequence.'" | '.$binVienna185.' -T 47.0 -d1 -e 99999  -noLP | tail -n +2' if ($version eq "1.8.5");
-$call = 'echo "'.$sequence.'" | '.$binVienna200.' -T 47.0 -d1 -e 99999 --noLP | tail -n +2' if ($version eq "2.0.0");
+$call = 'echo "'.$sequence.'" | '.$binVienna185.' -T 47.0 -'.$dangle.'  -'.$noLP.' -e 99999  -P '.$ParamFile.' | tail -n +2' if ($version eq "1.8.5");
+$call = 'echo "'.$sequence.'" | '.$binVienna200.' -T 47.0 -'.$dangle.' --'.$noLP.' -e 99999  -P '.$ParamFile.' | tail -n +2' if ($version eq "2.0.5");
 
 my %orig = ();
+print Dumper $call;
 foreach my $line (split(m/\n/, qx($call))) {
 	my ($str, $energy) = split(m/\s+/, $line);
 	my $noLPcheck = 1;
@@ -29,8 +31,11 @@ foreach my $line (split(m/\n/, qx($call))) {
 }
 
 my %gapc = ();
-foreach my $line (split(m/\n/, qx($binGAPC -t 47.0 "$sequence" | grep "," | cut -d " " -f 2,4))) {
+my $callGapc = $binGAPC.' -T 47.0 '.$sequence.' -P '.$ParamFile;
+#~ print Dumper $callGapc;
+foreach my $line (split(m/\n/, qx($callGapc | grep "," | cut -d " " -f 3,8))) {
 	my ($str, $energy) = split(m/\s+/, $line);
+	($energy) = ($energy =~ m/\((.+?),/) if ($grammar eq 'macrostate');
 	push @{$gapc{$str}}, sprintf("%.2f", $energy / 100);
 }
 
