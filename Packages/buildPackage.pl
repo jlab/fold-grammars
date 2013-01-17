e5009ed81f7f697e38dbbde5f8b9c1511975d7f2
Stefan Janssen <stefan.m.janssen@gmail.com>
2013-01-17 16:59:19 +0100

    build a debian package wrapper script.

---
+++ Packages/buildPackage.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+my @SERIES = ("lucid", "oneiric", "precise", "quantal");
+my $USER = 'Bielefeld BioInformatics Service';
+my $EMAIL = 'bibi-help@cebitec.uni-bielefeld.de';
+my $TMPDIR = 'tmpDir';
+my $KEYFINGERPRINT = '0x8FE66EF2';
+
+my ($package, $pathContainingDebian, $newversion, $comment) = @ARGV;
+
+if (defined $comment && $comment !~ m/^\s*$/) {
+	foreach my $series (@SERIES) {
+		qx(export DEBFULLNAME="$USER <$EMAIL>"; cd $pathContainingDebian && debchange --newversion $newversion-0ubuntu1 $comment --package $package --distribution $series);
+	}
+	my $currentDir = qx(pwd); chomp $currentDir;
+	qx(rm -rf $TMPDIR) if (-d $TMPDIR);
+	mkdir $TMPDIR;
+	my $packageDir = $TMPDIR.'/'.$package.'_'.$newversion;
+	mkdir $packageDir;
+	qx(hg clone ../ $packageDir/$package);
+	qx(cp -r $pathContainingDebian/debian $packageDir);
+	qx(cd $TMPDIR && tar czvf ${package}_$newversion.orig.tar.gz ${package}_$newversion);
+	qx(cd $TMPDIR/${package}_$newversion && debuild -S -k"$KEYFINGERPRINT");
+	print Dumper qx(dput ppa:bibi-help/bibitools $TMPDIR/${package}_$newversion-0ubuntu1_source.changes);
+	#~ qx(rm -rf $TMPDIR);
+} else {
+	die "no comment is given!\n";
+}
