fe725786f9d207ba281a85cb290545f1010efc90
Stefan Janssen <stefan.m.janssen@gmail.com>
2013-02-26 08:49:53 +0100

    forgot to adress target "install" in makefile for pKiss and RNAalishapes. Previously it was first and thus default target in the makefiles.

--- Misc/Packages/buildPackage.pl
+++ Misc/Packages/buildPackage.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Data::Dumper;
 
-my @SERIES = ("lucid", "oneiric", "precise", "quantal");
+my @SERIES = ("oneiric", "precise", "quantal"); #"lucid" too old gcc <= 4.4.x
 my $USER = 'Bielefeld BioInformatics Service';
 my $EMAIL = 'bibi-help@cebitec.uni-bielefeld.de';
 my $TMPDIR = 'tmpDir';
@@ -18,7 +18,7 @@ if (defined $comment && $comment !~ m/^\s*$/) {
 	mkdir $TMPDIR;
 	my $packageDir = $TMPDIR.'/'.$package.'_'.$newversion;
 	mkdir $packageDir;
-	print qx(hg clone ../ $packageDir/$package);
+	print qx(hg clone ../../ $packageDir/$package);
 	qx(cp -r $pathContainingDebian/debian $packageDir);
 	qx(cd $TMPDIR && tar czvf ${package}_$newversion.orig.tar.gz ${package}_$newversion);
 	
