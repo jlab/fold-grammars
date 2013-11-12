c0f0f830e6639c27dd93b267605a343f8b47775e
Stefan Janssen <stefan.m.janssen@gmail.com>
2013-11-12 16:43:21 +0100

    different gapc versions either put include <NAME> or include "NAME" in the generated code. I had to adapt my sed patterns to match both cases.

--- Misc/Applications/addRNAoptions.pl
+++ Misc/Applications/addRNAoptions.pl
@@ -22,7 +22,7 @@ open (IN, $infile) || die "can't read file '$infile': $!";
 			$line = <IN>; #: cat $(RTLIB)/generic_main.cc >> out_main.cc
 			$content .= $line;
 			if ($mode == 2) {
-				$content .= "\t".$sedBinary.' -i \'s|#include "rtlib/string.hh"|#include "bppm.hh"\n#include "rtlib/string.hh"|\' '.$1.'_main.cc'."\n";
+				$content .= "\t".$sedBinary.' -i \'s|#include .rtlib/string.hh.|#include "bppm.hh"\n#include "rtlib/string.hh"|\' '.$1.'_main.cc'."\n";
 				$content .= "\t".$sedBinary.' -i \'s|gapc::class_name obj;|gapc::class_name obj;\n  outside_gapc::class_name outside_obj;\n  std::stringstream outside_input;\n  outside_input << std::string(gapc::Opts::getOpts()->inputs.back().first) << '."'\\''+'\\''".' << std::string(gapc::Opts::getOpts()->inputs.back().first) << '."'\\''\\\\0'\\''".';\n  char* outside_argv[argc];\n  int seqIndex = -1;\n  for (int i = 0; i < argc; i++) {\n    if (strcmp(argv[i],gapc::Opts::getOpts()->inputs.back().first)==0) {\n      seqIndex = i;\n    }\n    outside_argv[i] = argv[i];\n  }\n  const std::string\& tmp = outside_input.str();\n  outside_argv[seqIndex] = const_cast<char*> (tmp.c_str());\n  gapc::Opts outside_opts;\n  try {\n    optind = 1;\n    outside_opts.parse(argc, outside_argv);\n  } catch (std::exception \&e) {\n      std::cerr << "Exception: " << e.what() << '."'\\''\\\\n'\\''".';\n      std::exit(1);\n  }\n|\' '.$1.'_main.cc'."\n";
 				$content .= "\t".$sedBinary.' -i \'s|obj.init(opts);|obj.init(opts);\n    outside_obj.init(outside_opts);|\' '.$1.'_main.cc'."\n";
 				$content .= "\t".$sedBinary.' -i \'s|gapc::return_type res = obj.run();|outside_obj.run();\n  gapc::return_type res = obj.run();|\' '.$1.'_main.cc'."\n";
@@ -32,8 +32,8 @@ open (IN, $infile) || die "can't read file '$infile': $!";
 			$content .= "\t".$sedBinary.' -i \'s|gapc::Opts opts;||\' '.$1.'_main.cc'."\n";
 			$content .= "\t".$sedBinary.' -i \'s|\\([^_]\\)opts\\.|\\1gapc::Opts::getOpts()->|g\' '.$1.'_main.cc'."\n";
 			$content .= "\t".$sedBinary.' -i \'s|obj.init(opts);|obj.init(\\*gapc::Opts::getOpts());|g\' '.$1.'_main.cc'."\n";
-			$content .= "\t".$sedBinary.' -i \'s|#include "rtlib/generic_opts.hh"|#include "Extensions/rnaoptions.hh"|\' '.$1.'_main.cc'."\n";
-			$content .= "\t".$sedBinary.' -i \'s%#include <rtlib/generic_opts.hh>%#include "Extensions/rnaoptions.hh"%\' '.$1.'.hh '.$1.'.cc'."\n";
+			$content .= "\t".$sedBinary.' -i \'s|#include .rtlib/generic_opts.hh.|#include "Extensions/rnaoptions.hh"|\' '.$1.'_main.cc'."\n";
+			$content .= "\t".$sedBinary.' -i \'s%#include .rtlib/generic_opts.hh.%#include "Extensions/rnaoptions.hh"%\' '.$1.'.hh '.$1.'.cc'."\n";
 			if ($mode == 1) { 
 				$content .= "\t".$sedBinary.' -i \'s|gapc::Opts::getOpts()->parse(argc, argv);|gapc::Opts::getOpts()->parse(argc, argv);\n\tif (gapc::Opts::getOpts()->inputs.size() == 2) {\n\t\tPairs::getGivenPairs()->setStructure(gapc::Opts::getOpts()->inputs.back());\n\t\tgapc::Opts::getOpts()->inputs.pop_back();\n\t}\n|\' '.$1.'_main.cc'."\n";
 			}
