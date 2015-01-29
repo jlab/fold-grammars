09ad34e869e180618e986a0591034cb8457d58a0
Stefan Janssen <stefan.m.janssen@gmail.com>
2015-01-29 13:01:24 +0100

    introduced the possibility to give different energy parameter files.

--- Misc/Analyses/Outside/cluster_runtime_ali.sh
+++ Misc/Analyses/Outside/cluster_runtime_ali.sh
@@ -10,6 +10,7 @@ len=`echo "5 * $SGE_TASK_ID" | bc`;
 inputFile=/vol/fold-grammars/src/Misc/Analyses/Testinputs/Outside/Runtime/Alignments/randomAlignment_$len.clustalW;
 bgapInputOutside=`/vol/perl-5.8.8/bin/perl /vol/fold-grammars/src/Misc/Applications/toBgapInput.pl $inputFile clustalW outside`;
 bgapInputInside=`/vol/perl-5.8.8/bin/perl /vol/fold-grammars/src/Misc/Applications/toBgapInput.pl $inputFile clustalW normal`;
+energyParameter=" -P /vol/cluster-data/sjanssen/ExpVar/rna_stefan2004.par ";
 
 uname -a
 echo "inputfile: $inputFile";
@@ -20,21 +21,21 @@ echo "length: $len";
 cd '/tmp';
 
 echo "#RNAalifold#"
-/vol/pi/bin/memtime64 /vol/pi/bin/RNAalifold-2.1.2 -d 0 2>&1 < "$inputFile" 
+/vol/pi/bin/memtime64 /vol/pi/bin/RNAalifold-2.1.2 $energyParameter -d 0 2>&1 < "$inputFile" 
 
 echo "#RNAalifold-p#"
-/vol/pi/bin/memtime64 /vol/pi/bin/RNAalifold-2.1.2 -d 0 -p 2>&1 < "$inputFile" 
+/vol/pi/bin/memtime64 /vol/pi/bin/RNAalifold-2.1.2 $energyParameter -d 0 -p 2>&1 < "$inputFile" 
 
 cd '/vol/cluster-data/sjanssen/bin/';
 
 echo "#ali_oa_i_nodangle_mfepp#"
-/vol/pi/bin/memtime64 /vol/cluster-data/sjanssen/bin/ali_oa_i_nodangle_mfepp -u 1 "$bgapInputInside" 2>&1
+/vol/pi/bin/memtime64 /vol/cluster-data/sjanssen/bin/ali_oa_i_nodangle_mfepp $energyParameter -u 1 "$bgapInputInside" 2>&1
 
 echo "#ali_oa_i_nodangle_pfunc#"
-/vol/pi/bin/memtime64 /vol/cluster-data/sjanssen/bin/ali_oa_i_nodangle_pfunc -u 1 "$bgapInputInside" 2>&1
+/vol/pi/bin/memtime64 /vol/cluster-data/sjanssen/bin/ali_oa_i_nodangle_pfunc $energyParameter -u 1 "$bgapInputInside" 2>&1
 
 echo "#ali_oa_o_nodangle_pfunc#"
-/vol/pi/bin/memtime64 /vol/cluster-data/sjanssen/bin/ali_oa_o_nodangle_pfunc -u 1 -o=/dev/null "$bgapInputOutside" 2>&1
+/vol/pi/bin/memtime64 /vol/cluster-data/sjanssen/bin/ali_oa_o_nodangle_pfunc $energyParameter -u 1 -o=/dev/null "$bgapInputOutside" 2>&1
 
 #qsub -cwd -l arch=sol-amd64 -l hostname="fuc*" runRuntimeAnalysisAlignment.sh
 
--- Misc/Analyses/Outside/cluster_runtime_ss.sh
+++ Misc/Analyses/Outside/cluster_runtime_ss.sh
@@ -9,6 +9,7 @@
 seqLen=`echo "5 * $SGE_TASK_ID" | bc`;
 sequenceFile=/vol/fold-grammars/src/Misc/Analyses/Testinputs/Outside/Runtime/Singlesequences/randomSequence_$seqLen.fasta;
 sequence=`/vol/gnu/bin/cat $sequenceFile | /vol/gnu/bin/tail -n1`;
+energyParameter=" -P /vol/cluster-data/sjanssen/ExpVar/rna_stefan2004.par ";
 
 uname -a
 echo "sequencefile: $sequenceFile";
@@ -18,20 +19,20 @@ echo "seqLength: $seqLen";
 cd '/tmp';
 
 echo "#RNAfold#"
-/vol/pi/bin/memtime64 /vol/pi/bin/RNAfold-2.1.2 -d 0 2>&1 < "$sequenceFile" 
+/vol/pi/bin/memtime64 /vol/pi/bin/RNAfold-2.1.2 $energyParameter -d 0 2>&1 < "$sequenceFile" 
 
 echo "#RNAfold-p#"
-/vol/pi/bin/memtime64 /vol/pi/bin/RNAfold-2.1.2 -d 0 -p 2>&1 < "$sequenceFile" 
+/vol/pi/bin/memtime64 /vol/pi/bin/RNAfold-2.1.2-P $energyParameter  -d 0 -p 2>&1 < "$sequenceFile" 
 
 cd '/vol/cluster-data/sjanssen/bin/';
 
 echo "#oa_i_nodangle_mfepp#"
-/vol/pi/bin/memtime64 /vol/cluster-data/sjanssen/bin/oa_i_nodangle_mfepp -u 1 "$sequence" 2>&1
+/vol/pi/bin/memtime64 /vol/cluster-data/sjanssen/bin/oa_i_nodangle_mfepp -P $energyParameter -u 1 "$sequence" 2>&1
 
 echo "#oa_i_nodangle_pfunc#"
-/vol/pi/bin/memtime64 /vol/cluster-data/sjanssen/bin/oa_i_nodangle_pfunc -u 1 "$sequence" 2>&1
+/vol/pi/bin/memtime64 /vol/cluster-data/sjanssen/bin/oa_i_nodangle_pfunc -P $energyParameter -u 1 "$sequence" 2>&1
 
 echo "#oa_o_nodangle_pfunc#"
-/vol/pi/bin/memtime64 /vol/cluster-data/sjanssen/bin/oa_o_nodangle_pfunc -u 1 -o=/dev/null "$sequence+$sequence" 2>&1
+/vol/pi/bin/memtime64 /vol/cluster-data/sjanssen/bin/oa_o_nodangle_pfunc -P $energyParameter -u 1 -o=/dev/null "$sequence+$sequence" 2>&1
 
 #qsub -cwd -l arch=sol-amd64 -l hostname="fuc*" runRuntimeAnalysis.sh
