120d4a72b0a8a6df1d6cbdd278153c8776cd2971
Stefan Janssen <stefan.m.janssen@gmail.com>
2016-06-15 15:00:46 -0700

    avoid redefinition of sub

--- Misc/Applications/lib/foldGrammars/ClusterSettings.pm
+++ Misc/Applications/lib/foldGrammars/ClusterSettings.pm
@@ -4,8 +4,19 @@ use strict;
 use warnings;
 
 package ClusterSettings;
-
 my $cluster = "SANDIEGO";
+
+sub getMemLimit {
+	my ($limit_in_GB) = @_;
+	return "" if (not defined $limit_in_GB);
+	if ($cluster eq 'BIELEFELD') {
+		return ' -l virtual_free='.$limit_in_GB.'GB -l h_vmem='.$limit_in_GB.'GB ';
+	} elsif ($cluster eq 'SANDIEGO') {
+		return ' -l pmem='.$limit_in_GB.'GB ';
+	} else {
+		die "unkown cluster configuration!\n";
+		}
+}
 
 if ($cluster eq "BIELEFELD") {
 	our $PREFIX_QSUB_PARAMETER = '$';
@@ -13,24 +24,14 @@ if ($cluster eq "BIELEFELD") {
 	our $SH = '/usr/bin/sh';
 	our $ARCH = ' -l arch=lx24-amd64 ';
 	our $PARAM_WORK_DIR = '-cwd';
-
-	sub getMemLimit {
-		my ($limit_in_GB) = @_;
-		return "" if (not defined $limit_in_GB);
-		return ' -l virtual_free='.$limit_in_GB.'GB -l h_vmem='.$limit_in_GB.'GB ';
-	}
 } elsif ($cluster eq 'SANDIEGO') {
 	our $PREFIX_QSUB_PARAMETER = 'PBS';
 	our $JOBID = 'PBS_ARRAYID';
 	our $SH = '/bin/sh';
 	our $ARCH = '';
 	our $PARAM_WORK_DIR = '-d';
-
-	sub getMemLimit {
-		my ($limit_in_GB) = @_;
-		return "" if (not defined $limit_in_GB);
-		return ' -l pmem='.$limit_in_GB.'GB ';
-	}
 } else {
 	die "unkown cluster configuration!\n";
 }
+
+1;
