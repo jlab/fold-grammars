533a576d448a7698d0f36e151a25dc54a5075a63
Stefan Janssen <stefan.m.janssen@gmail.com>
2016-06-15 14:55:04 -0700

    added a file to configure the cluster

--- Misc/Applications/lib/foldGrammars/ClusterSettings.pm
+++ Misc/Applications/lib/foldGrammars/ClusterSettings.pm
@@ -5,14 +5,32 @@ use warnings;
 
 package ClusterSettings;
 
-our $PREFIX_QSUB_PARAMETER = 'PBS';
-our $JOBID = 'PBS_ARRAYID';
-our $SH = '/bin/sh';
-our $ARCH = '';
-our $PARAM_WORK_DIR = '-d';
+my $cluster = "SANDIEGO";
 
-sub getMemLimit {
-	my ($limit_in_GB) = @_;
-	return "" if (not defined $limit_in_GB);
-	return ' -l pmem='.$limit_in_GB.'GB ';
+if ($cluster eq "BIELEFELD") {
+	our $PREFIX_QSUB_PARAMETER = '$';
+	our $JOBID = 'SGE_TASK_ID';
+	our $SH = '/usr/bin/sh';
+	our $ARCH = ' -l arch=lx24-amd64 ';
+	our $PARAM_WORK_DIR = '-cwd';
+
+	sub getMemLimit {
+		my ($limit_in_GB) = @_;
+		return "" if (not defined $limit_in_GB);
+		return ' -l virtual_free='.$limit_in_GB.'GB -l h_vmem='.$limit_in_GB.'GB ';
+	}
+} elsif ($cluster eq 'SANDIEGO') {
+	our $PREFIX_QSUB_PARAMETER = 'PBS';
+	our $JOBID = 'PBS_ARRAYID';
+	our $SH = '/bin/sh';
+	our $ARCH = '';
+	our $PARAM_WORK_DIR = '-d';
+
+	sub getMemLimit {
+		my ($limit_in_GB) = @_;
+		return "" if (not defined $limit_in_GB);
+		return ' -l pmem='.$limit_in_GB.'GB ';
+	}
+} else {
+	die "unkown cluster configuration!\n";
 }
