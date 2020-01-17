26ad656485e6d8613b7826c524e92f5b9a1da0be
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-01-17 15:23:54 +0100

    adding test sequences for Richardson 2016

--- Misc/Test-Suite/StefanStyle/test_modifications.py
+++ Misc/Test-Suite/StefanStyle/test_modifications.py
@@ -61,5 +61,6 @@ run_cmd('make -C ../../Applications/RNAcofold -j $NUMCPUS eval')
 execute_comparison('Truth/modfold_hudson2013.mfa', BIN_GAPC, FP_PARAM)
 execute_comparison('Truth/modfold_wright2018.mfa', BIN_GAPC, FP_PARAM)
 execute_comparison('Truth/modfold_wright2007.mfa', BIN_GAPC, FP_PARAM)
+execute_comparison('Truth/modfold_richardson2016.mfa', BIN_GAPC, FP_PARAM)
 
 print("modfold tests passed")
