5d5b9e19d4c941f98809cf8ca4820cf822456e3b
Katharina Maibach <Katharina.Maibach@bio.uni-giessen.de>
2020-02-21 14:44:36 +0100

    adapt travis tasks, create subdirectory

--- Misc/Test-Suite/KatharinaStyle/runTests.py
+++ Misc/Test-Suite/KatharinaStyle/runTests.py
@@ -3,7 +3,6 @@
 import unittest
 import os
 import subprocess
-
 import tempfile
 # import shutil
 
@@ -11,13 +10,9 @@ import tempfile
 class Test(unittest.TestCase):
 
     def setUp(self):
-        # os.chdir('/homes/kmaibach/GAPC/fold-grammars/')  # set working directory
-
         self.fp_workdir = tempfile.mkdtemp()
         os.system('basedir=`pwd` && cd "%s" && gapc -p "alg_dotBracket*alg_count" $basedir/../../nodangle.gap -I $basedir/../../' % self.fp_workdir)
         os.system('basedir=`pwd` && cd "%s" && make -f out.mf CPPFLAGS_EXTRA="-I $basedir/../../"' % self.fp_workdir)
-
-        # self.tmp_dir = tempfile.mkdtemp()  # create temporary directory
 
         self.sequences = [
             "CCCAa+aaaUGGG",
@@ -74,20 +69,13 @@ class Test(unittest.TestCase):
 
     def testUnambiguousness(self):
         for seq in self.sequences:
-            # index = self.sequences.index(seq)
             cmd = ('%s/out "%s"' % (self.fp_workdir, seq))
             dot_count = [(subprocess.check_output(cmd, shell=True)).decode("utf-8")]
-            # file_name = os.path.join('/homes/kmaibach/GAPC/test/', str(index) + ".txt")
-            # file = open(file_name, "w")
             for element in dot_count:
-                # file.write(element)
                 dot_count_list = element.split('\n')
-                # split_list = []
                 for el in dot_count_list:
                     if not (el == 'Answer: ' or el == ''):
-                        # split_list.append(el.split(",")[1])
                         self.assertEqual(el.split(",")[1], ' 1 )')
-            # file.close()
 
 
 if __name__ == "__main__":
