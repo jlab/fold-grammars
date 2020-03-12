b72ec692d11c9b3fe89e2d654eadd6c3f7e51c24
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-03-12 13:38:24 +0100

    extensions for shape building in cofold grammars

--- Extensions/cofold.hh
+++ Extensions/cofold.hh
@@ -31,4 +31,46 @@ inline void appendSeperatorRegion(String &res, const Basic_Subsequence<alphabet,
 	}
 }
 
+template<typename alphabet, typename pos_type>
+inline Rope seperatorRegion_to_shape(const Basic_Subsequence<alphabet, pos_type> &seq, int shapelevel)
+{
+	unsigned int num_leading_bases = 0;
+	for (unsigned int k = seq.i; k < seq.j; ++k) {
+		if (seq[k] == SEPARATOR_BASE) {
+			break;
+		}
+		++num_leading_bases;
+	}
+	Rope res;
+	if ((shapelevel <= 2) && (num_leading_bases > 0)) {
+		append(res, '_');
+	}
+	if (num_leading_bases < seq.j - seq.i) {
+		append(res, '+');
+	}
+	if ((shapelevel <= 2) && (seq.j - seq.i - num_leading_bases > 0)) {
+		append(res, '_');
+	}
+	return res;
+}
+
+// If Rope size is >= 2, returns contents from second to secondlast character
+// used to obtain "y" from "[y]"
+template<typename X>
+inline Rope inner(const rope::Ref<X> &str) {
+	rope::Ref<X> &x = const_cast<rope::Ref<X>&>(str);
+	typename rope::Ref<X>::iterator it = x.begin();
+	Rope res;
+	if (str.size() <= 2) {
+		res.empty();
+	} else {
+		++it;
+		for (unsigned int i = 1; i+1 < str.size(); ++i) {
+			append(res, (char) *it);
+			++it;
+		}
+	}
+	return res;
+}
+
 #endif
