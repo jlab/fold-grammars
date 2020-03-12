0449fe6d5a6fb64d90c9b08b2124b9b519230a73
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-03-12 16:46:01 +0100

    adding Rope function

--- Extensions/shapes.hh
+++ Extensions/shapes.hh
@@ -4,4 +4,23 @@
 static const shape_t openParen = '[';
 static const shape_t closeParen = ']';
 
+// If Rope size is >= 2, returns contents from second to secondlast character
+// used to obtain "y" from "[y]"
+template<typename X>
+inline Rope inner(const rope::Ref<X> &str) {
+	rope::Ref<X> &x = const_cast<rope::Ref<X>&>(str);
+	typename rope::Ref<X>::iterator it = x.begin();
+	Rope res;
+	if (str.size() <= 2) {
+		res.empty();
+	} else {
+		++it;
+		for (unsigned int i = 1; i+1 < str.size(); ++i) {
+			append(res, (char) *it);
+			++it;
+		}
+	}
+	return res;
+}
+
 #endif
