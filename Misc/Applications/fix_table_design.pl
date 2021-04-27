b9f4ace6a156ae8cb1f8cbd1bdde2889dfa1c161
Stefan Janssen <stefan.m.janssen@gmail.com>
2021-04-27 13:15:59 +0200

    adding table designs for other grammars

--- Misc/Applications/fix_table_design.pl
+++ Misc/Applications/fix_table_design.pl
@@ -15,7 +15,13 @@ my ($gapc_file, $gapc_options) = @ARGV;
 my $tabledesign = "";
 if (($gapc_options =~ m/--window-mode/) && ($gapc_options =~ m/--k?backtrace/)) {
 	if ($gapc_file =~ m/ali_[nodangle|overdangle]/) {
-		$tabledesign = "--tab iloop --tab leftB --tab ml_comps --tab ml_comps1 --tab multiloop --tab rightB --tab stack --tab struct";
+		$tabledesign = "--tab leftB --tab ml_comps --tab ml_comps1 --tab multiloop --tab rightB --tab stack --tab strong --tab struct";
+	} elsif ($gapc_file =~ m/ali_microstate/) {
+		$tabledesign = "--tab leftB --tab ml_comps --tab ml_comps1 --tab multiloop --tab rightB --tab stack --tab iloop --tab struct";
+	} elsif ($gapc_file =~ m/ali_macrostate/) {
+		$tabledesign = "--tab ml_comps1 --tab no_dl_no_ss_end --tab dl_or_ss_left_ss_end --tab no_dl_ss_end --tab iloop --tab ml_comps2 --tab ml_comps4 --tab strong --tab left_dangle --tab nodangle --tab block_dl --tab dl_or_ss_left_no_ss_end --tab noleft_dangle --tab weak --tab block_dlr --tab left_unpaired --tab ml_comps3";
+	} elsif ($gapc_file =~ m/ali_pKiss/) {
+		$tabledesign = "--tab struct --tab strong --tab weak --tab stack --tab leftB --tab rightB --tab iloop --tab multiloop --tab ml_comps --tab ml_comps1 --tab mldangle --tab pk_comps --tab middleNoDangling --tab help_pknot_free_hk --tab help_pkiss_Aleft --tab help_pkiss_Aright  --tab help_pknot_free_hk_3D --tab help_pkiss_C --tab help_pkiss_D --tab knot --tab strategyA --tab strategyB --tab strategyC --tab strategyD --tab pknotsRG";
 	}
 }
 print $tabledesign;
