a275162722d6646667a164f3a0bfe9979b45b353
Stefan Janssen <stefan.m.janssen@gmail.com>
2022-02-17 09:02:48 +0100

    closing resources + parsing bp-prop from *.ps files

--- Misc/Test-Suite/PythonStyle/test_utils.py
+++ Misc/Test-Suite/PythonStyle/test_utils.py
@@ -3,6 +3,7 @@ import sys
 import os
 import pandas as pd
 import numpy as np
+import tempfile
 
 sys.path.append(os.path.abspath("./"))
 import nodangle as nd
@@ -11,19 +12,23 @@ def getArchTripe():
     with subprocess.Popen("gcc -dumpmachine", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as P:
         return P.stdout.read().decode('ascii').split('\n')[0]
 
-def getBackpropTruth(inputseq, alg='pfunc', base='Misc/Test-Suite/PythonStyle/'):
+def getBackpropTruth(inputseq, alg='pfunc', base='Misc/Test-Suite/PythonStyle/', verbose=sys.stderr):
     """Uses a specially compiled nodangle enum instance that tracks used NTs to manually count candidates using NTs at (i,j) positions"""
     arch = getArchTripe()
     if not os.path.exists('%s%s/backprop' % (base, arch)):
-        print("compiling gapc binary", file=sys.stderr)
+        if verbose is not None:
+            verbose.write("compiling gapc binary\n")
         with subprocess.Popen("cd %s && make backprop" % base, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as P:
             if P.wait() != 0:
                 err_msg = P.stderr.read()
                 if err_msg != b"":
                     raise ValueError(err_msg.decode('ascii'))
 
-    print("execute (alg %s) %s%s/backprop '%s'" % (base, arch, alg, inputseq), file=sys.stderr)
-    lines = os.popen('%s%s/backprop -u 1 "%s"' % (base, arch, inputseq)).read().split('\n')
+    if verbose is not None:
+        verbose.write("execute (alg %s) %s%s/backprop '%s'" % (base, arch, alg, inputseq))
+    P = os.popen('%s%s/backprop -u 1 "%s"' % (base, arch, inputseq))
+    lines = P.read().split('\n')
+    P.close()
 
     usedNTs = dict()
     for line in lines[1:-1]:
@@ -49,9 +54,43 @@ def getBackpropTruth(inputseq, alg='pfunc', base='Misc/Test-Suite/PythonStyle/')
                  usedNTs[ntname].loc[int(i),int(j)] += 1
     return usedNTs
 
-def testBackprop(inpseq):
+def getBasePairTruth(inputseq, base='Misc/Test-Suite/PythonStyle/', verbose=sys.stderr):
+    """Uses outside_nodangle grammar to compute base pair probabilities, i.e. backprop for weak*strong/pfunc"""
+    arch = getArchTripe()
+    if not os.path.exists('%s%s/outside' % (base, arch)):
+        if verbose is not None:
+            verbose.write("compiling gapc binary")
+        with subprocess.Popen("cd %s && make outside" % base, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as P:
+            if P.wait() != 0:
+                err_msg = P.stderr.read()
+                if err_msg != b"":
+                    raise ValueError(err_msg.decode('ascii'))
+
+    _, fp_dotPlot = tempfile.mkstemp()
+    if verbose is not None:
+        verbose.write("execute %s%s/outside '%s' -o '%s'" % (base, arch, inputseq, fp_dotPlot))
+    P = os.popen('%s%s/outside -u 1 "%s+%s" -o "%s"' % (base, arch, inputseq, inputseq, fp_dotPlot))
+    lines = P.read().split('\n')
+    P.close()
+    res = pd.DataFrame(data=np.nan, index=range(len(inputseq)+1), columns=range(len(inputseq)+1))
+    with open(fp_dotPlot, 'r') as f:
+        probsstarted = False
+        for line in f.readlines():
+            if line.startswith('%start of base pair probability data'):
+                probsstarted = True
+                continue
+            if line.startswith('showpage'):
+                probsstarted = False
+                continue
+            if probsstarted:
+                i,j,prob,_ = line.split()
+                res.loc[int(i)-1,int(j)] = float(prob)**2
+    os.remove(fp_dotPlot)
+    return res
+
+def testBackprop(inputseq, verbose=sys.stderr):
     # initialize tables and other stuff
-    nd.init(inpseq, printstack=False, taball=True, printBTstack=False)
+    nd.init(inputseq, printstack=False, taball=True, printBTstack=False)
 
     # trigger forward pass
     py = nd.nt_struct(0)
@@ -60,14 +99,14 @@ def testBackprop(inpseq):
     nd.tables['struct'].bt_set(0,0,1.0)
 
     # obtain Truth from gapc binary
-    usedNTs = getBackpropTruth(inpseq, alg=nd.ALGEBRA)
+    usedNTs = getBackpropTruth(inputseq, alg=nd.ALGEBRA, verbose=verbose)
 
     error = False
     report = ""
     for nt in sorted(usedNTs.keys()):
         report += "=== %s ===\n" % nt
-        for i in range(len(inpseq)):
-            js = range(i, len(inpseq))
+        for i in range(len(inputseq)):
+            js = range(i, len(inputseq))
             if nt == 'struct':
                 js = [0]
             for j in js:
@@ -82,5 +121,40 @@ def testBackprop(inpseq):
     if error is True:
         print(report)
     else:
-        print("backprop test '%s' passed." % inpseq)
+        if verbose is not None:
+            verbose.write("backprop test '%s' passed." % inputseq)
+    return error
+
+def testBasepair(inputseq, verbose=sys.stderr):
+    # initialize tables and other stuff
+    nd.init(inputseq, printstack=False, taball=True, printBTstack=False)
+
+    # trigger forward pass
+    pfunc = nd.nt_struct(0)
+
+    # initialize recursion base for backward pass
+    nd.tables['struct'].bt_set(0,0,1.0)
+
+    # obtain Truth from gapc outside_nodangle binary
+    exp = getBasePairTruth(inputseq, verbose=verbose)
+
+    error = False
+    report = ""
+    for i in range(len(inputseq)+1):
+        for j in range(i,len(inputseq)+1):
+            if pd.notnull(exp.loc[i,j]):
+                fwd = nd.tables['weak'].array.loc[i,j]
+                bwd = nd.backtrace(i,j,'weak')
+                obs = fwd*bwd/pfunc
+
+                correct = np.isclose(exp.loc[i,j], obs)
+                error = error or (not correct)
+
+                report += '*' if correct else '!'
+                report += ' (%i,%i) %f %f\n' % (i,j, exp.loc[i,j], obs)
+    if error is True:
+        print(report)
+    else:
+        if verbose is not None:
+            verbose.write("basepair prop. test '%s' passed." % inputseq)
     return error
