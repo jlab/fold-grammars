7931c14cfa7e2b1d1a9454909caafefebfdb0f81
Fynn Muermanns <fynn.m@live.de>
2022-10-14 16:01:34 +0200

    store previously calculated results in an array instead of a hashmap

--- Extensions/probing.hh
+++ Extensions/probing.hh
@@ -1,14 +1,20 @@
 #ifndef PROBING_HH
 #define PROBING_HH
 
-
-#define FASTER_getReactivityScore // turn on/off to switch between old/new implementation
-//#define PRINT_INFO
-
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <time.h>
+
+#define ARRAYLOOKUP // use array to store calc results
+#ifdef ARRAYLOOKUP
+//#define PRECALC // precalculate everything and store results in array (use in combination w/ ARRAYLOOKUP)
+#endif
+
+//#define HASHLOOKUP // use hashmap to store calc results
+#ifdef HASHLOOKUP
+//#define PRINT_INFO // turn on to print debugging information in hash lookup version
+#endif
 
 /*
   Fynn's improvements/suggestions:
@@ -25,13 +31,20 @@
 	-GetReactivityScore:
 		-pass "probingData" vector (and "modifier" string) to calculateScore by reference (makes code about 0.05x- 0.1x faster)
 		-replace reversing of probingData to off_probingData w/ 
-		vector<double> off_probingData(probingData.rbegin(), probingData.rend()); probingData.clear();
+		 vector<double> off_probingData(probingData.rbegin(), probingData.rend()); probingData.clear();
   
   -major adjustments:
-	-GetReactivityScore:
+	-GetReactivityScore (hash version):
 		-keep track of already calculated scores by hashing the function parameters and
 	  	performing a simple hashmap lookup to see if the function was aleady called once
-	  	w/ the same parameters; if so, simply return the previously calculated result (makes code about 5x faster)
+	  	w/ the same parameters (separately for inputSubseq and offsetSubseq);
+		if so, simply return the previously calculated result 
+		-makes code about about 5-10x faster, speedup seems to grow expontentially w/ input
+
+	-GetReactivityScore (array version):
+		-store previously calculated results in one (single track) or two (two track) matrices
+		-and simply retrieve the previously calculated results if they exist
+		-makes code about 1.5-2x faster the the hash version
 */
 
 //Stefans own 1-dimensional k-means clustering
@@ -244,7 +257,7 @@ inline double calculateScore(const Subsequence &Base, const bool isUnpaired, con
 	return score;
 }
 
-#ifdef FASTER_getReactivityScore
+#ifdef HASHLOOKUP
 
 #include <unordered_map>
 #include <boost/functional/hash.hpp>
@@ -253,7 +266,8 @@ inline double getReactivityScore(const Subsequence &inputSubseq, const bool isUn
 {
 	static int callCounter = 0;
 	static int couldSkipCounter = 0;
-	static std::unordered_map<std::size_t, double> prevScores; // store previously calculated scores in here
+	static std::unordered_map<std::size_t, double> iSubseqScores; // store previously calculated scores in here
+	static std::unordered_map<std::size_t, double> oSubseqScores; // store previously calculated scores in here
 
 	static bool isLoaded = false;
   	static std::vector<double> off_probingData;
@@ -268,23 +282,18 @@ inline double getReactivityScore(const Subsequence &inputSubseq, const bool isUn
 	callCounter++;
 
 	// incrementally constuct a unique key/hash from the function parameters
-	#ifdef FASTER_getReactivityScore
-	std::size_t key = 0;
-	boost::hash_combine(key, inputSubseq.i); // i and j positions of Subsequence object uniquely identify it
-	boost::hash_combine(key, inputSubseq.j);
-	boost::hash_combine(key, isUnpaired);
-	boost::hash_combine(key, offsetSubseq.i);
-	boost::hash_combine(key, offsetSubseq.j);
-	boost::hash_combine(key, offset);
+	std::size_t iKey = 0;
+	std::size_t oKey = 0;
 
-	if (prevScores.find(key) != prevScores.end()) 
-	{
-		// if the hashkey exists, return the previously calculated result
-		// instead of recalculating it again
-		couldSkipCounter++;
-		return prevScores[key];
-	}
-	#endif
+	// build hash for inputSubSeq scores
+	boost::hash_combine(iKey, inputSubseq.i); // i and j positions of Subsequence object uniquely identify it
+	boost::hash_combine(iKey, inputSubseq.j);
+	boost::hash_combine(iKey, isUnpaired);
+
+	// build hash for offsetSubSeq scores
+	boost::hash_combine(oKey, offsetSubseq.i);
+	boost::hash_combine(oKey, offsetSubseq.j);
+	boost::hash_combine(oKey, isUnpaired);
   
 	if (!isLoaded) {
 		std::string line;
@@ -310,10 +319,10 @@ inline double getReactivityScore(const Subsequence &inputSubseq, const bool isUn
 		std::vector<std::pair<const char*, unsigned> > inputsSequences = getInputs();
 					
 		if (offset) {
-		inputsLength = inputsSequences.at(0).second + inputsSequences.at(1).second;
+		inputsLength = inputsSequences[0].second + inputsSequences[1].second;
 		} 
 		else {
-		inputsLength = inputsSequences.at(0).second;
+		inputsLength = inputsSequences[0].second;
 		}
 		
 		if (probingData.size() < inputsLength) {
@@ -333,7 +342,7 @@ inline double getReactivityScore(const Subsequence &inputSubseq, const bool isUn
 			int k;
 			for (i = 0; i < numData; i++) {
 				k = i;
-				if (offset && (i >= (int) inputsSequences.at(0).second || i >= sep))
+				if (offset && (i >= (int) inputsSequences[0].second || i >= sep))
 				{ 
 					base = offsetSubseq;
 					k = i - sep;
@@ -399,11 +408,34 @@ inline double getReactivityScore(const Subsequence &inputSubseq, const bool isUn
 		isLoaded = true;
 	}
 	
-	if (offset) {
-		score = calculateScore(offsetSubseq, isUnpaired, off_probingData, clusterPaired, clusterUnpaired, modifier) + calculateScore(inputSubseq, isUnpaired, probingData, clusterPaired, clusterUnpaired, modifier);
+	// check if inputSubseq score was already computed once before
+	if (iSubseqScores.find(iKey) != iSubseqScores.end())
+	{
+		score = iSubseqScores[iKey];
 	}
-	else {
-	score = calculateScore(inputSubseq, isUnpaired, probingData, clusterPaired, clusterUnpaired, modifier);
+	else
+	{
+		double iSubseqScore = calculateScore(inputSubseq, isUnpaired, probingData, 
+		                                     clusterPaired, clusterUnpaired, modifier);
+		score = iSubseqScore;
+		iSubseqScores[iKey] = iSubseqScore;
+
+	}
+	
+	// check if offsetSubseq score was already computed once before
+	if (offset) 
+	{
+		if (oSubseqScores.find(oKey) != oSubseqScores.end())
+		{
+			score += oSubseqScores[oKey];
+		}
+		else
+		{
+			double offsetSubseqScore = calculateScore(offsetSubseq, isUnpaired, off_probingData, 
+			                                          clusterPaired, clusterUnpaired, modifier);
+			score += offsetSubseqScore;
+			oSubseqScores[oKey] = offsetSubseqScore;
+		}
 	}
 
 	#ifdef PRINT_INFO
@@ -432,16 +464,231 @@ inline double getReactivityScore(const Subsequence &inputSubseq, const bool isUn
 		}
 		printf("\nScore: %f\n\n", score);
 	}
-	#endif 
-
-	#ifdef FASTER_getReactivityScore
-	prevScores[key] = score; // add the score to the map for potential lookups in another function call
 	#endif
 
 	return score;
 }
-#else
+#elif defined(ARRAYLOOKUP)
 
+#ifdef PRECALC
+inline double calculateScoreAdj(const unsigned int Base_i, const unsigned int Base_j, const Subsequence &Base,
+                                const bool isUnpaired, const std::vector<double>& probingData, const double clusterPaired, 
+								const double clusterUnpaired, const std::string& modifier)
+{
+	double score = 0.0;
+
+  	for (unsigned int i = Base_i; i < Base_j && i < probingData.size(); i++)
+	{
+		if ((modifier == "DMS") && (Base[i] != A_BASE) && (Base[i] != C_BASE)) continue;
+    	if ((modifier == "CMCT") && (Base[i] != U_BASE) && (Base[i] != G_BASE)) continue;
+
+		if (strcmp(getProbing_normalization(), "centroid") == 0)
+		{
+			if (isUnpaired) score += fabs(probingData[i] - clusterUnpaired);
+			else score += fabs(probingData[i] - clusterPaired);
+		} 
+		else
+		{
+			if (isUnpaired) score += probingData[i];
+			else score -= probingData[i];
+		}
+	}
+
+	return score;
+}
+#endif
+
+inline double getReactivityScore(const Subsequence &inputSubseq, const bool isUnpaired, const Subsequence &offsetSubseq, const bool offset) {
+	static bool isLoaded = false;
+	static std::vector<double> off_probingData;
+	static std::vector<double> probingData;
+
+	unsigned int iLen = inputSubseq.seq->n;
+	unsigned int oLen = offsetSubseq.seq->n;
+	unsigned long iIndex = (iLen * iLen * isUnpaired) + inputSubseq.i * iLen + inputSubseq.j;
+	unsigned long oIndex = (oLen * oLen * isUnpaired) + offsetSubseq.i * oLen + offsetSubseq.j;
+
+	// allocate n*n matrix as single dimensional array and initialize values to 0
+	static double* iSubseqScores = new double[iLen*iLen*2]();
+	static double* oSubseqScores = new double[oLen*oLen*2]();
+
+	int sep = -1;
+	double score;
+
+	static double clusterUnpaired;
+	static double clusterPaired;
+	std::string modifier = getProbing_modifier();
+	
+	if (!isLoaded) 
+	{
+		std::string line;
+		std::ifstream infile (getProbing_dataFilename());
+		if (infile.is_open()) 
+		{
+			while (getline (infile,line)) {
+				char *thisLine = strdup(line.c_str());
+				//we expect each line to hold the base position (starting with 1) and the reactivity.
+				if (strcmp(thisLine,"&")==0){
+					sep = probingData.size();
+					continue;
+				}
+				strtok(thisLine, " \t");
+				double reactivity = atof(strtok(NULL, " \t"));	
+				probingData.push_back(reactivity);
+			}
+			infile.close();
+		}
+			
+		// TODO (kmaibach): needs reworking when changing the way the whitespace between two sequences is represented
+		unsigned int inputsLength = 0;
+		std::vector<std::pair<const char*, unsigned> > inputsSequences = getInputs();
+					
+		if (offset) {
+		inputsLength = inputsSequences[0].second + inputsSequences[1].second;
+		} 
+		else {
+		inputsLength = inputsSequences[0].second;
+		}
+		
+		if (probingData.size() < inputsLength) {
+		std::cerr << "Warning: chemical probing data file '" << getProbing_dataFilename() << "' misses " << (inputsLength - probingData.size()) << " data-row(s) " << std::endl << "         compared to the number of nucleotides in your input sequence." << std::endl << "         Missing values will be set to 0.0!" << std::endl;
+		}
+		if (probingData.size() > inputsLength) {
+		std::cerr << "Warning: chemical probing data file '" << getProbing_dataFilename() << "' contains " << (probingData.size()-inputsLength) << " more row(s) " << std::endl << "         than there are nucleotides in your input sequence." << std::endl << "         Exceeding data lines will be ignored!" << std::endl;	
+		}
+			
+		// centroid normalization
+		if (strcmp(getProbing_normalization(), "centroid") == 0) 
+		{		
+			int numData = probingData.size();
+			Subsequence base = inputSubseq;
+			double *data = new double[numData];
+			int i = 0;
+			int j = 0;
+			int k;
+			for (i = 0; i < numData; i++)
+			{
+				k = i;
+				if (offset && (i >= (int) inputsSequences[0].second || i >= sep))
+				{ 
+					base = offsetSubseq;
+					k = i - sep;
+				
+				}
+				if ((modifier == "DMS") && (base[k] != A_BASE) && (base[k] != C_BASE)) {
+					continue;
+				}
+				if ((modifier == "CMCT") && (base[k] != U_BASE) && (base[k] != G_BASE)) {
+					continue;
+				}
+				if (probingData[i] < 0) {
+					data[j] = 0.0;
+					probingData[i] = 0.0;
+				} 
+				else {
+					data[j] = probingData[i];
+				}
+				j++;
+			}
+
+			double centroids[2];
+			kmeans(2, j, data, centroids);
+			clusterUnpaired = centroids[0];
+			clusterPaired = centroids[1];
+			delete[] data;
+		}
+	
+		if (strcmp(getProbing_normalization(), "RNAstructure") == 0) {
+			for(std::vector<double>::iterator it = probingData.begin(); it != probingData.end(); it++) {
+				*it = CalculatePseudoEnergy(*it,modifier,getProbing_slope(),getProbing_intercept()); //the parameters are: plain reactivity, modifier type, slope, intercept
+			}
+		}
+		if (strcmp(getProbing_normalization(), "logplain") == 0) {
+			for(std::vector<double>::iterator it = probingData.begin(); it != probingData.end(); it++) {
+				if (*it+1.0 < 0.0) {
+					*it = 0.0;
+				} 
+				else {
+					*it = log(*it+1.0);
+				}
+			}
+		}
+		if ((strcmp(getProbing_normalization(), "asProbabilities") == 0)) {
+			double max = 0.0;
+			for(std::vector<double>::iterator it = probingData.begin(); it != probingData.end(); it++) {
+				if (max < *it) max = *it;
+				if (*it < 0.0) *it = 0.0;
+			}
+			if (max > 0.0) {
+				for(std::vector<double>::iterator it = probingData.begin(); it != probingData.end(); it++) {
+					*it = ((int) ((*it / max) * 10.0)) / 10.0;
+				}
+			}
+		}
+		
+		if (sep > -1)
+		{
+			off_probingData = std::vector<double>(probingData.rbegin(), probingData.rend());
+			probingData.clear();
+		}
+
+		isLoaded = true;
+
+		#ifdef PRECALC
+		for (unsigned int i=0; i<iLen-1; i++)
+		{
+			for (unsigned int j=i+1; j<iLen; j++) 
+			{
+				unsigned long iIndex = (iLen * iLen * isUnpaired) + i * iLen + j;
+				iSubseqScores[iIndex] = calculateScoreAdj(i, j, inputSubseq, isUnpaired, probingData, clusterPaired, clusterUnpaired, modifier);
+			}
+		}
+		for (unsigned int i=0; i<oLen-1; i++)
+		{
+			for (unsigned int j=i+1; j<oLen; j++)
+			{
+				unsigned long oIndex = (oLen * oLen * isUnpaired) + i * oLen + j;
+				oSubseqScores[oIndex] = calculateScoreAdj(i, j, offsetSubseq, isUnpaired, off_probingData, clusterPaired, clusterUnpaired, modifier);
+			}
+		}
+		#endif
+	}
+	
+	#ifdef PRECALC
+	score = iSubseqScores[iIndex];
+	if (offset) score += oSubseqScores[oIndex];
+	
+	#else
+	if (iSubseqScores[iIndex]) // check if score was already calculated once (meaning if value at index isn't 0 anymore)
+	{
+		score = iSubseqScores[iIndex];
+	}
+	else
+	{
+		double iSubseqScore = calculateScore(inputSubseq, isUnpaired, probingData, clusterPaired, clusterUnpaired, modifier);
+		score = iSubseqScore;
+		iSubseqScores[iIndex] = iSubseqScore;
+	}
+
+	if (offset)
+	{
+		if (oSubseqScores[oIndex])
+		{
+			score += oSubseqScores[oIndex];
+		}
+		else
+		{
+			double oSubseqScore = calculateScore(offsetSubseq, isUnpaired, off_probingData, clusterPaired, clusterUnpaired, modifier);
+			score += oSubseqScore;
+			oSubseqScores[oIndex] = oSubseqScore;
+		}
+	}
+	#endif
+
+	return score;
+}
+
+#else
 inline double getReactivityScore(const Subsequence &inputSubseq, const bool isUnpaired, const Subsequence &offsetSubseq, const bool offset) {
   static bool isLoaded = false;
   static std::vector<double> off_probingData;
@@ -477,9 +724,9 @@ inline double getReactivityScore(const Subsequence &inputSubseq, const bool isUn
     std::vector<std::pair<const char*, unsigned> > inputsSequences = getInputs();
                 
     if (offset) {
-      inputsLength = inputsSequences.at(0).second + inputsSequences.at(1).second;
+      inputsLength = inputsSequences[0].second + inputsSequences[1].second;
     } else {
-      inputsLength = inputsSequences.at(0).second;
+      inputsLength = inputsSequences[0].second;
     }
        
     if (probingData.size() < inputsLength) {
@@ -500,7 +747,7 @@ inline double getReactivityScore(const Subsequence &inputSubseq, const bool isUn
 	    for (i = 0; i < numData; i++) {
 		    k = i;
 		    if (offset) {
-		      if(i >= (int) inputsSequences.at(0).second || i >= sep) {
+		      if(i >= (int) inputsSequences[0].second || i >= sep) {
 				    base = offsetSubseq;
 					  k = i - sep;
 			    }
@@ -511,11 +758,11 @@ inline double getReactivityScore(const Subsequence &inputSubseq, const bool isUn
 			  if ((modifier == "CMCT") && (base[k] != U_BASE) && (base[k] != G_BASE)) {
 			    continue;
 			  }
-			  if (probingData.at(i) < 0) {
+			  if (probingData[i] < 0) {
 			    data[j] = 0.0;
-				  probingData.at(i) = 0.0;
+				  probingData[i] = 0.0;
 			  } else {
-				  data[j] = probingData.at(i);
+				  data[j] = probingData[i];
 			  }
         j++;
       }
@@ -556,7 +803,7 @@ inline double getReactivityScore(const Subsequence &inputSubseq, const bool isUn
         
 	  if (sep > -1){
 		  for (int i=probingData.size()-1; i>=sep; i--){
-			  off_probingData.insert(off_probingData.begin(), probingData.at(i));
+			  off_probingData.insert(off_probingData.begin(), probingData[i]);
 			  probingData.erase(probingData.begin() + i);
 		  }
 	  }
@@ -583,3 +830,5 @@ inline double getReactivityScore(const Subsequence &inputSubseq, const bool isUn
 }
 
 #endif
+
+
