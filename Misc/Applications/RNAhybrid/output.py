6cce31818691f5bbafab15331e253d076c500169
Stefan Janssen <stefan.m.janssen@gmail.com>
2025-02-26 09:26:01 +0100

    report energy necessary to break target internal pairings to make bases accessible for miRNA hybridization

--- Misc/Applications/RNAhybrid/output.py
+++ Misc/Applications/RNAhybrid/output.py
@@ -7,6 +7,8 @@ def print_single_answer(answer, number:int, out=sys.stdout):
     out.write("miRNA : %s\n" % answer['mirna_name'])
     out.write("length: %i\n" % answer['mirna_length'])
     out.write("mfe: %.1f kcal/mol\n" % (answer['mfe'] / 100))
+    if 'broken_target_substructure_energy' in answer.keys():
+        out.write('energy lost to make target accessible: %.1f kcal/mol\n' % ((answer['broken_target_substructure_energy'] - answer['original_target_substructure_energy']) / 100))
     out.write("p-value: %f\n" % answer["p-value"])
     out.write("5' seed length: %s\n" % answer['stacklen'])
     out.write("miRNA bulge length: %s\n" % answer['bulgelen'])
