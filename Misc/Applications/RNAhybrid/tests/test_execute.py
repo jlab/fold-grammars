c2eece6f1d58a8b531656fcad62b0f4ad30794cb
Stefan Janssen <stefan.m.janssen@gmail.com>
2025-02-26 14:24:28 +0100

    fix path names

--- Misc/Applications/RNAhybrid/tests/test_execute.py
+++ Misc/Applications/RNAhybrid/tests/test_execute.py
@@ -1,11 +1,14 @@
 import sys
-from os.path import dirname
+from os.path import dirname, join
 sys.path.append(dirname(__file__) + '/../')
 
 from unittest import TestCase, main
 
 from parse import *
 from execute import read_CT_file, disentangle_knots
+
+def getFP(filepath):
+    return join(dirname(__file__), filepath)
 
 class TestExecute(TestCase):
     def setUp(self):
@@ -15,23 +18,23 @@ class TestExecute(TestCase):
         pass
 
     def test_read_CT_file(self):
-        obs = read_CT_file('tests/data/SARS-CoV-2_Full_Length_Secondary_Structure_Map.ct')
+        obs = read_CT_file(getFP('data/SARS-CoV-2_Full_Length_Secondary_Structure_Map.ct'))
         self.assertEqual('wPKs', obs['name'])
         self.assertEqual(29903, len(obs['sequence']))
         self.assertTrue(obs['sequence'].endswith('ACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'))
 
         with self.assertRaises(ValueError) as e:
-            obs = read_CT_file('tests/data/broken.ct')
+            obs = read_CT_file(getFP('data/broken.ct'))
 
         with self.assertRaises(ValueError) as e:
-            obs = read_CT_file('tests/data/broken_missing.ct')
+            obs = read_CT_file(getFP('data/broken_missing.ct'))
 
     def test_disentangle_knots(self):
-        obs = disentangle_knots(read_CT_file('tests/data/earlyKnot.ct')['structure'], verbose=None)
+        obs = disentangle_knots(read_CT_file(getFP('data/earlyKnot.ct'))['structure'], verbose=None)
         self.assertEqual(len(obs['nested']), 6)
         self.assertEqual(len(obs['knotted']), 3)
 
-        obs = disentangle_knots(read_CT_file('tests/data/lateKnot.ct')['structure'], verbose=None)
+        obs = disentangle_knots(read_CT_file(getFP('data/lateKnot.ct'))['structure'], verbose=None)
         self.assertEqual(len(obs['nested']), 6)
         self.assertEqual(len(obs['knotted']), 3)
 
